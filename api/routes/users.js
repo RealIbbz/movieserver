c404021a7561afa286b45f296ce1e268c72013f3
Kelvin Ibbs <kelvini@thearck.com.au>
2021-06-15 13:30:06 +1000

    Remove unused routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -10,7 +10,6 @@ module.exports = function (app) {
       body('email').isLength({ min: 3, max: 100 }).isEmail().normalizeEmail(),
       body('name').isLength({ min: 1, max: 200 }).withMessage('Name must be between 1 and 200 characters'),
       body('password').isLength({ min: 5, max: 50 }).withMessage('Password must be between 5 and 50 characters'),
-      UsersController.createUser])
-    .get(UsersController.createUser);
+      UsersController.createUser]);
 
 };
